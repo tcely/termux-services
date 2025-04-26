e715032d811679b823d571e980cea4a9597e2f9f
tcely <tcely@users.noreply.github.com>
2025-04-25 23:43:13 -0400

    Update start-services.fish

--- start-services.fish
+++ start-services.fish
@@ -1,6 +1,19 @@
-if status --is-login
+if status is-login
+    function default
+        for value in $argv
+            if test -z "$value"
+                continue
+            end
+            echo $value
+            return 0
+        end
+        return 1
+    end
+
+    set -l prefix "$(default "$TERMUX__PREFIX" "$PREFIX")"
+
     # For more details, see set(1)
-    set -gx SVDIR "${TERMUX__PREFIX}/var/service"
-    set -gx LOGDIR "${TERMUX__PREFIX}/var/log"
+    set -gx SVDIR "$prefix"/var/service
+    set -gx LOGDIR "$prefix"/var/log
     service-daemon start >/dev/null 2>&1 &
 end
