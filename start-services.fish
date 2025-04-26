3458694fffe422d9b9cb9c4ed474171abdbf8c1e
tcely <tcely@users.noreply.github.com>
2025-04-25 22:29:57 -0400

    Update start-services.fish

--- start-services.fish
+++ start-services.fish
@@ -1,6 +1,6 @@
 if status --is-login
     # For more details, see set(1)
-    set -gx SVDIR $PREFIX/var/service
-    set -gx LOGDIR $PREFIX/var/log
+    set -gx SVDIR "${TERMUX__PREFIX}/var/service"
+    set -gx LOGDIR "${TERMUX__PREFIX}/var/log"
     service-daemon start >/dev/null 2>&1 &
 end
